9d6ba263686d24ba37aa5ec31d3f4db97d1f119d
Varsni-S <varsu84@gmail.com>
2022-04-21 13:31:57 +0530

    first commit

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,16 +1,72 @@
-import React from 'react';
-import {Text, View, TextInput, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, TextInput, StyleSheet, FlatList} from 'react-native';
+import staticPosts from '../StaticDatas/Datas';
 
 export default function SearchScreen() {
+  const [search, setSearch] = useState('');
+
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState(staticPosts);
+
+  const searchFilterFunction = text => {
+    if (text) {
+      const newData = masterDataSource.filter(function (item) {
+        const itemData = item.profileName
+          ? item.profileName.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      console.log(newData);
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      //setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({item}) => {
+    console.log(item, 'item');
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.profileName.toUpperCase()}
+      </Text>
+    );
+  };
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+  const getItem = item => {
+    // Function for click on an item
+    alert('Id : ' + item.id + ' Name : ' + item.profileName);
+  };
   return (
     <View style={{flex: 1, backgroundColor: 'black'}}>
+      {/* <View style={styles.container}> */}
       <TextInput
         style={styles.textInputStyle}
-        // onChangeText={text => searchFilterFunction(text)}
-        // value={search}
+        onChangeText={text => searchFilterFunction(text)}
+        value={search}
         underlineColorAndroid="transparent"
         placeholder="Search"
         placeholderTextColor="white"
+      />
+      <FlatList
+        data={filteredDataSource}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={ItemView}
+        ItemSeparatorComponent={ItemSeparatorView}
       />
     </View>
   );
@@ -25,5 +81,22 @@ const styles = StyleSheet.create({
     borderColor: 'grey',
     backgroundColor: 'black',
     borderRadius: 10,
+    color: 'white',
   },
+
+  container: {
+    backgroundColor: 'white',
+  },
+  itemStyle: {
+    padding: 10,
+    color: 'white',
+  },
+  // textInputStyle: {
+  //   height: 40,
+  //   borderWidth: 1,
+  //   paddingLeft: 20,
+  //   margin: 5,
+  //   borderColor: '#009688',
+  //   backgroundColor: '#FFFFFF',
+  // },
 });
--- src/StaticDatas/Datas.js
+++ src/StaticDatas/Datas.js
@@ -1,38 +1,43 @@
 const staticPosts = [
   {
+    id: 1,
     contentText: 'Good Vibes !',
     imageUrl: {
       uri: 'https://cdn.pixabay.com/photo/2017/06/01/08/30/rolls-royce-2362821__340.jpg',
     },
-    profileName: '_Varsni_',
+    profileName: 'Varsni',
   },
   {
+    id: 2,
     contentText: 'Smile',
     imageUrl: {
       uri: 'https://cdn.pixabay.com/photo/2016/04/07/06/53/bmw-1313343__340.jpg',
     },
-    profileName: 'jeevatha____',
+    profileName: 'jeevatha',
   },
   {
+    id: 3,
     contentText: 'Psychic',
     imageUrl: {
       uri: 'https://cdn.pixabay.com/photo/2020/09/09/13/03/bike-riding-5557589__340.png',
     },
-    profileName: 'dharani_._',
+    profileName: 'dharani',
   },
   {
+    id: 4,
     contentText: '',
     imageUrl: {
       uri: 'https://cdn.pixabay.com/photo/2015/03/09/18/34/beach-666122__340.jpg',
     },
-    profileName: '__island__',
+    profileName: 'island',
   },
   {
+    id: 5,
     contentText: '',
     imageUrl: {
       uri: 'https://cdn.pixabay.com/photo/2020/02/07/19/05/spaceship-4828098__340.jpg',
     },
-    profileName: '-- tanika --',
+    profileName: 'Tanika',
   },
 ];
 
