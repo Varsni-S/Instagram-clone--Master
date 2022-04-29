9d077329adaa3cc631543c6cf9e77dcfd3549482
Varsni-S <varsu84@gmail.com>
2022-04-29 07:42:44 +0530

    video reels

--- src/Components/Homescreen/Reels.js
+++ src/Components/Homescreen/Reels.js
@@ -5,29 +5,45 @@ import SingleReel from './SingleReel';
 export default function Reels() {
   const videoData = [
     {
+      id: 1,
       video: require('../../assests/videos/video1.mp4'),
       title: 'Varsni',
+      imageUrl: {
+        uri: 'https://cdn.pixabay.com/photo/2016/04/07/06/53/bmw-1313343__340.jpg',
+      },
       description: 'feels the beauty',
       likes: '454',
       isLike: false,
     },
     {
+      id: 2,
       video: require('../../assests/videos/video2.mp4'),
+      imageUrl: {
+        uri: 'https://cdn.pixabay.com/photo/2017/06/01/08/30/rolls-royce-2362821__340.jpg',
+      },
       title: 'jeevitha',
       description: 'nice looks',
       likes: '654',
       isLike: false,
     },
     {
+      id: 3,
       video: require('../../assests/videos/video3.mp4'),
       title: 'tanika',
+      imageUrl: {
+        uri: 'https://cdn.pixabay.com/photo/2020/09/09/13/03/bike-riding-5557589__340.png',
+      },
       description: 'good capture',
       likes: '554',
       isLike: false,
     },
     {
+      id: 4,
       video: require('../../assests/videos/video4.mp4'),
       title: 'Aishu',
+      imageUrl: {
+        uri: 'https://cdn.pixabay.com/photo/2015/03/09/18/34/beach-666122__340.jpg',
+      },
       description: 'keep rocking',
       likes: '224',
       isLike: false,
--- src/Components/Homescreen/SingleReel.js
+++ src/Components/Homescreen/SingleReel.js
@@ -1,15 +1,199 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Dimensions, Image} from 'react-native';
+import React, {useRef, useState} from 'react';
+import Video from 'react-native-video';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {
+  faHeart,
+  faHeartCircleBolt,
+  faMusic,
+  faVolumeMute,
+} from '@fortawesome/free-solid-svg-icons';
+import {useNavigation} from '@react-navigation/native';
 
-export default function SingleReel() {
+export default function SingleReel({item, index, currentIndex}) {
+  const navigation = useNavigation();
+
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
+  const videoRef = useRef(null);
+
+  const onBuffer = buffer => {
+    console.log('buffring', buffer);
+  };
+  const onError = error => {
+    console.log('error', error);
+  };
+
+  const [mute, setMute] = useState(false);
+  const [like, setLike] = useState(false);
+
+  const likeToggled = () => {
+    setLike(!like);
+  };
   return (
-    <View>
-      <Text
+    <View
+      style={{
+        width: windowWidth,
+        height: windowHeight,
+        position: 'relative',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <TouchableOpacity
+        activeOpacity={0.9}
+        onPress={() => setMute(!mute)}
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+        }}>
+        {/* <Video
+          videoRef={videoRef}
+          onBuffer={onBuffer}
+          onError={onError}
+          repeat={true}
+          resizeMode="cover"
+          paused={currentIndex == index ? false : true}
+          source={item.video}
+          muted={mute}
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+          }}
+        /> */}
+      </TouchableOpacity>
+      <FontAwesomeIcon
+        icon={faVolumeMute}
+        style={{
+          fontSize: mute ? 20 : 0,
+          color: 'white',
+          position: 'absolute',
+          backgroundColor: 'rgba(52,52,52,0.6)',
+          borderRadius: 100,
+          padding: mute ? 20 : 0,
+        }}
+      />
+      {/* <Text
         style={{
           color: 'white',
         }}>
         SingleReel
-      </Text>
+      </Text> */}
+      <View
+        style={{
+          position: 'absolute',
+          width: windowWidth,
+          zIndex: 1,
+          bottom: 0, //edited
+          padding: 10,
+        }}>
+        <View>
+          <TouchableOpacity style={{width: 150}}>
+            <View
+              style={{width: 100, flexDirection: 'row', alignItems: 'center'}}>
+              <View
+                style={{
+                  width: 32,
+                  height: 32,
+                  borderRadius: 100,
+                  backgroundColor: 'white',
+                  margin: 10,
+                }}>
+                <Image
+                  source={item.imageUrl}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    resizeMode: 'cover',
+                    borderRadius: 100,
+                  }}
+                />
+              </View>
+              <Text style={{color: 'white', fontSize: 16}}>{item.title}</Text>
+            </View>
+          </TouchableOpacity>
+          <Text style={{color: 'white', fontSize: 14, marginHorizontal: 10}}>
+            {item.description}
+          </Text>
+          <View style={{flexDirection: 'row', padding: 10}}>
+            <FontAwesomeIcon
+              icon={faMusic}
+              style={{color: 'white', fontSize: 16}}
+            />
+            <Text style={{color: 'white', marginLeft: 10}}>Original Audio</Text>
+          </View>
+        </View>
+      </View>
+
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 20,
+          right: 0,
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}>
+        <TouchableOpacity style={{padding: 10}} onPress={likeToggled}>
+          <FontAwesomeIcon
+            icon={faHeart}
+            size={22}
+            color={like ? 'red' : 'white'}
+          />
+          <Text style={{fontSize: 18, color: 'white', marginLeft: -2}}>
+            {like ? 500 : 499}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 10}}>
+          <FontAwesomeIcon
+            icon="comment"
+            size={22}
+            color="white"
+            onPress={() => {
+              navigation.navigate('Comments');
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 10}}>
+          <FontAwesomeIcon
+            icon="paper-plane"
+            size={22}
+            color="white"
+            onPress={() => {
+              navigation.navigate('MessagingScreen');
+            }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={{padding: 10}}>
+          <FontAwesomeIcon
+            icon="ellipsis-v"
+            size={22}
+            color="white"
+            //  onPress={moreFunction}
+          />
+        </TouchableOpacity>
+        <View
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 10,
+            borderWidth: 2,
+            borderColor: 'white',
+            margin: 10,
+          }}>
+          <Image
+            source={item.imageUrl}
+            style={{
+              width: '100%',
+              height: '100%',
+              borderRadius: 10,
+              resizeMode: 'cover',
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
