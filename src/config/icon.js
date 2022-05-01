a7ca3f7d6038ae9f9fdb557feaeb21097744eff3
Varsni-S <varsu84@gmail.com>
2022-05-01 19:34:11 +0530

    photos del

--- src/config/icon.js
+++ src/config/icon.js
@@ -1,17 +1,5 @@
 export default {
   images: {
-    // heartIcon : require('../../assets/like.png'),
-    // commentIcon : require('../../assets/comment.png'),
-    // shareIcon : require('../../assets/share.png'),
-    // homeIcon : require('../../assets/home.png'),
-    // searchIcon : require('../../assets/search.png'),
-    // reelsIcon : require('../../assets/reels.png'),
-    // profileIcon : require('../../assets/profile.png'),
-    // messageIcon : require('../../assets/message.png'),
-    // ellipsisIcon : require('../../assets/ellipisis.png'),
-    // downIcon : require('../../assets/down.png'),
-    // plusRoundedIcon : require('../../assets/icons8-add-new-50.png'),
-    // menuIcon : require('../../assets/icons8-menu-rounded-50.png'),
     addIcon: require('../assests/icons8-add-32.png'),
   },
 };
